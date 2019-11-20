e0e63983df5397cd2d01b10b5f51cabad459d2f6
BSAraujo <breno-7-rj@hotmail.com>
2019-11-20 12:28:21 -0300

    Add structure SBM

--- src/sbm.jl
+++ src/sbm.jl
@@ -1,39 +1,64 @@
+struct SBM
+    w::Matrix{Float64}      # Matrix of probabilities
+    dist::String            # Distribution (either "poisson" or "bernoulli")
+    n_communities::Int      # Number of communities/clusters
 
-function generate(probability_matrix::Array{Float64,2}, n_per_community::Array{Int,2};
-                            distribution::String="poisson", seed::Any=nothing)
+    # Constructor function for struct SBM
+    function SBM(w::Matrix{Float64}, dist::String)
+        # Check if w is a square matrix
+        if size(w,1) != size(w,2)
+            throw(ArgumentError("Matrix w must be a square matrix."))
+        end
+        # Check if elements in w are strictly positive
+        if any(w .< 0)
+            throw(DomainError("All elements in matrix of probabilities w must be strictly positive."))
+        end
+        # Check if matrix is symmetric
+        if w != w'
+            throw(ArgumentError("Matrix w must be symmetric."))
+        end
+        q = size(w,1)
+
+        # Check input argument 'distribution'
+        if ~(dist in ["poisson","bernoulli"])
+            throw(ArgumentError(string("Invalid distribution: ", dist)))
+        end
+        if dist == "bernoulli"
+            # Check if w is a matrix of 0-1 values
+            if any(w .> 1)
+                throw(DomainError("When using a bernoulli distribution all elements in matrix of probabilities w must be between 0 and 1."))
+            end
+        end
+        return new(w, dist, q)
+    end
+end
+
+function generate(sbm::SBM, n_per_community::Vector{Int}, seed::Any=nothing)::Matrix{Int}
     """
-    Generates a graph from the Stochastic Block Model (Poisson version).
+    Generates a graph from the Stochastic Block Model.
 
     Parameters
     ----------
-    probability_matrix : Array{Float64,2}
-        Matrix of expected number of edges under Poisson distribution.
-    n_per_community : Array{Int64,2}
+    sbm : SBM
+        An SBM structure.
+    n_per_community : Vector{Int}
         Number of nodes in each community.
-    distribution : String
-        Distribution to be used for generating edges (either "poisson" or "bernoulli").
     seed : Any
         Seed for the random number generator.
 
     Returns
     -------
-    adj_matrix : Array{Int64,2}
+    adj_matrix : Matrix{Int}
         Adjacency matrix of the generated graph
     """
-
-    # Check input argument 'distribution'
-    if ~(distribution in ["poisson","bernoulli"])
-        throw(ArgumentError(string("Invalid distribution: ", distribution)))
-    end
     # Check input argument 'n_per_community'
     if any(n_per_community .<= 0)
         throw(DomainError(string("Invalid value in argument n_per_community: ",
                                  "the number of nodes in each community must be strictly positive.")))
     end
-    # Check input argument 'probability_matrix'
-    if any(probability_matrix .< 0)
-        throw(DomainError(string("Invalid value in argument probability_matrix: ",
-                                 "the probability values must be strictly positive.")))
+    # Check if n_per_community matches the dimensions of w
+    if ~(length(n_per_community) == sbm.n_communities)
+        throw(ArgumentError("Number of communities in n_per_community must match the number of communities in SBM"))
     end
 
     # Set the random seed if one was provided
@@ -47,14 +72,14 @@ function generate(probability_matrix::Array{Float64,2}, n_per_community::Array{I
     # Construct adjacency matrix
     for i=1:n_vertices
         for j=1:i
-            proba = probability_matrix[community_labels[i], community_labels[j]];
-            if distribution == "poisson"
+            proba = sbm.w[community_labels[i], community_labels[j]];
+            if sbm.dist == "poisson"
                 if i == j
                     proba = proba / 2 # "The factor of half is included solely because it makes the algebra easier"
                 end
                 pois = Distributions.Poisson(proba);
                 adj_matrix[i,j] = rand(pois);
-            elseif distribution == "bernoulli"
+            elseif sbm.dist == "bernoulli"
                 if i == j
                     adj_matrix[i,j] = 0;
                 else
@@ -70,4 +95,29 @@ function generate(probability_matrix::Array{Float64,2}, n_per_community::Array{I
 
     # Return adjacency matrix
     return adj_matrix
+end
+
+function generate(probability_matrix::Matrix{Float64}, n_per_community::Vector{Int};
+                            distribution::String="poisson", seed::Any=nothing)::Matrix{Int}
+    """
+    Generates a graph from the Stochastic Block Model.
+
+    Parameters
+    ----------
+    probability_matrix : Matrix{Float64}
+        Matrix of expected number of edges under Poisson distribution.
+    n_per_community : Vector{Int}
+        Number of nodes in each community.
+    distribution : String
+        Distribution to be used for generating edges (either "poisson" or "bernoulli").
+    seed : Any
+        Seed for the random number generator.
+
+    Returns
+    -------
+    adj_matrix : Matrix{Int}
+        Adjacency matrix of the generated graph
+    """
+    sbm = SBM(probability_matrix, distribution)
+    return generate(sbm, n_per_community, seed)
 end
--- test/test_sbm.jl
+++ test/test_sbm.jl
@@ -1,6 +1,19 @@
 using StochasticBlockModel
 using Test
 
+@testset "SBM struct" begin
+
+    w = [1.0 0.3; 0.3 1.2]
+    sbm = StochasticBlockModel.SBM(w, "poisson")
+    @test sbm.w == w
+    @test sbm.dist == "poisson"
+    @test sbm.n_communities == 2
+
+    @test_throws ArgumentError StochasticBlockModel.SBM([1.0 0; 0 1; 9 1], "poisson")
+    @test_throws DomainError StochasticBlockModel.SBM([1.0 0; 0 -1], "bernoulli")
+    @test_throws DomainError StochasticBlockModel.SBM([1.0 0; 0 1.2], "bernoulli")
+end
+
 @testset "Generate graphs" begin
 
     expected_A = [0  1  2  1  0  0  0  0
@@ -11,7 +24,7 @@ using Test
                  0  0  0  0  1  0  1  1
                  0  0  0  0  2  1  0  2
                  0  0  0  0  1  1  2  0]
-    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4 4], seed=2) == expected_A
+    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4; 4], seed=2) == expected_A
 
     expected_A = [0  1  2  0  0  0
                   1  0  1  0  0  0
@@ -19,7 +32,7 @@ using Test
                   0  0  0  0  1  1
                   0  0  0  1  0  0
                   0  0  0  1  0  2]
-    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [3 3], seed=2) == expected_A
+    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [3; 3], seed=2) == expected_A
 
     expected_A = [0  1  1  1  0  0  0  0
                   1  0  1  1  0  0  0  0
@@ -29,16 +42,17 @@ using Test
                   0  0  0  0  1  0  1  1
                   0  0  0  0  1  1  0  1
                   0  0  0  0  1  1  1  0]
-    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4 4], distribution="bernoulli", seed=2) == expected_A
+    @test StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4; 4], distribution="bernoulli", seed=2) == expected_A
 
     # Test for invalid argument for "distribution"
-    @test_throws ArgumentError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4 4], distribution="a", seed=2)
+    @test_throws ArgumentError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4; 4], distribution="a", seed=2)
 
     # Test for invalid argument for "n_per_community"
-    @test_throws DomainError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4 -4], distribution="poisson", seed=2)
-    @test_throws DomainError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [0 2], distribution="bernoulli", seed=2)
+    @test_throws DomainError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [4; -4], distribution="poisson", seed=2)
+    @test_throws DomainError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [0; 2], distribution="bernoulli", seed=2)
+    @test_throws ArgumentError StochasticBlockModel.generate([1.0 0.0; 0.0 1.0], [1; 2; 4], distribution="bernoulli", seed=2)
 
     # Test for invalid argument for "probability_matrix"
-    @test_throws DomainError StochasticBlockModel.generate([1.0 -0.2; 0.0 1.0], [4 4], distribution="poisson", seed=2)
-    @test_throws DomainError StochasticBlockModel.generate([-1.0 0.0; 0.0 1.0], [2 2], distribution="bernoulli", seed=2)
+    @test_throws DomainError StochasticBlockModel.generate([1.0 -0.2; 0.0 1.0], [4; 4], distribution="poisson", seed=2)
+    @test_throws DomainError StochasticBlockModel.generate([-1.0 0.0; 0.0 1.0], [2; 2], distribution="bernoulli", seed=2)
 end
